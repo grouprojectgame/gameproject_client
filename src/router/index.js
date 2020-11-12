4d3e1b689e783657416c85fd8f30c9bcb3c569e1
daryalfuad <daryalfuad@gmail.com>
2020-11-12 23:05:36 +0700

    masukin sfx dan page winner

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import WelcomePage from '../views/WelcomePage.vue'
+import WinnerPage from '../views/Winner.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'WelcomePage',
     component: WelcomePage
+  },
+  {
+    path: '/',
+    name: 'WinnerPage',
+    component: WinnerPage
   }
 ]
 
