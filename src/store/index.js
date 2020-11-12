9fffb1c989efd63a4f9678c686592c4b08c66a68
risyadazhary <Risyad01@gmail.com>
2020-11-13 00:02:57 +0700

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -9,24 +9,7 @@ export default new Vuex.Store({
   },
   mutations: {
 
-    addPlayer (context, payload) {
-      return axios({
-        method: 'POST',
-        url: '/players',
-        data: {
-          name: payload,
-          score: 0
-        }
-      })
-        .then(({ data }) => {
-          console.log(data)
-          context.commit('ADD_ID', data.id)
-          return data
-        })
-        .catch(err => {
-          console.log(err)
-        })
-    }
+
 
   },
   actions: {
