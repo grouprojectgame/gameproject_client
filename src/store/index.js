b95fc9560c89823268b43fa9e61893f4dcfce4b8
risyadazhary <Risyad01@gmail.com>
2020-11-13 00:03:25 +0700

    add route for player

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,24 @@ export default new Vuex.Store({
   },
   mutations: {
 
-
+    addPlayer (context, payload) {
+      return axios({
+        method: 'POST',
+        url: '/players',
+        data: {
+          name: payload,
+          score: 0
+        }
+      })
+        .then(({ data }) => {
+          console.log(data)
+          context.commit('ADD_ID', data.id)
+          return data
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
 
   },
   actions: {
